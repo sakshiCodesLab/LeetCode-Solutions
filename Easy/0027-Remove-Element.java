f2ee2c32a6f682400d09f7e9ca467a7f0d796e19
Sakshi <sakshiyuvi92@gmail.com>
2026-08-01 12:08:11 +0530

    Add Remove Element Solution

---
+++ Easy/0027-Remove-Element.java
@@ -0,0 +1,18 @@
+// LeetCode 27: Remove Element
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+            return k;
+        }
+    
+}
