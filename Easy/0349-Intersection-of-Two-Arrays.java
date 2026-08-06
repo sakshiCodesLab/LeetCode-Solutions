f7bd995eebf1fa408df47a7a69a10dec548473f3
Sakshi <sakshiyuvi92@gmail.com>
2026-08-06 21:59:27 +0530

    Add Intersection of Two Arrays Solution

---
+++ Easy/0349-Intersection-of-Two-Arrays.java
@@ -0,0 +1,26 @@
+// LeetCode 349: Intersection of Two Arrays
+// https://leetcode.com/problems/intersection-of-two-arrays/
+
+import java.util.*;
+
+class Solution {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        for (int num : nums1) {
+            set.add(num);
+        }
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                result.add(num);
+            }
+        }
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for (int num : result) {
+            ans[i++] = num;
+        }
+        return ans;
+    }
+}
