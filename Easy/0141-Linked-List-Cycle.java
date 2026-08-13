5979af5cd3b0513c71fafc4c485f414eed4ee894
Sakshi <sakshiyuvi92@gmail.com>
2026-08-13 17:40:13 +0530

    Add Linked List Cycle Solution

---
+++ Easy/0141-Linked-List-Cycle.java
@@ -0,0 +1,36 @@
+// LeetCode 141: Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
