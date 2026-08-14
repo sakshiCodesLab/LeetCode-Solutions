52e4c38b400e0feddccd22434cb9066977f3b54d
Sakshi <sakshiyuvi92@gmail.com>
2026-08-14 17:35:08 +0530

    Add Guess Number Higher or Lower Solution

---
+++ Easy/0374-Guess-Number-Higher-or-Lower.java
@@ -0,0 +1,35 @@
+// LeetCode 374:  Guess Number Higher or Lower
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+/**
+ * Forward declaration of guess API.
+ *
+ * @param num your guess
+ * @return -1 if num is higher than the picked number 1 if num is lower than the
+ * picked number 0 if num is equal to the picked number
+ */
+class Solution extends GuessGame {
+
+    public int guessNumber(int n) {
+
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;
+            } else if (result == -1) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
