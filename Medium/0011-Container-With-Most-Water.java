df16a96b9ac8dd73d9451658e109c9b1f94f84ad
Sakshi <sakshiyuvi92@gmail.com>
2026-08-18 13:26:34 +0530

    Add Container With Most Water Solution

---
+++ Medium/0011-Container-With-Most-Water.java
@@ -0,0 +1,23 @@
+// LeetCode 11: Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+
+class Solution {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = width * currentHeight;
+            maxWater = Math.max(maxWater, currentArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+}
