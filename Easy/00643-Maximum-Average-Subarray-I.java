71c747b5217d1b4bb339b2d80db2975e0ff36fce
Sakshi <sakshiyuvi92@gmail.com>
2026-08-22 13:09:34 +0530

    Update Maximum Average Subarray I solution

--- Easy/00643-Maximum-Average-Subarray-I.java
+++ Easy/00643-Maximum-Average-Subarray-I.java
@@ -1,3 +1,19 @@
-// LeetCode 374:  Maximum Average Subarray I
+// LeetCode 643:  Maximum Average Subarray I
 // https://leetcode.com/problems/maximum-average-subarray-I/
 
+class Solution {
+
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i];
+            sum -= nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+    }
+}
