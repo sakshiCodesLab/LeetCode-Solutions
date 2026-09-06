d7dae551300be0a2d4131a5daa6cfdaf83f735af
Sakshi <sakshiyuvi92@gmail.com>
2026-09-06 14:48:41 +0530

    Add Kth Largest Element in an Array Solution

---
+++ Medium/0215-Kth-Largest-Element-in-an-Array.java
@@ -0,0 +1,19 @@
+// LeetCode 215: Kth Largest Element in an Array
+// https://leetcode.com/problems/Kth-largest-element-in-an-array/
+
+import java.util.*;
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue <Integer> pq=new PriorityQueue<>();
+        for(int num:nums)
+        {
+            pq.add(num);
+        
+        if(pq.size()>k)
+        {
+            pq.poll();
+        }
+        }
+        return pq.peek();
+    }
+}
