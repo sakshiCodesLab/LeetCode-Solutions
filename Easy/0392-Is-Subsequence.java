137003c779ccf7fdabd0d043d3e8f9b44a3944fc
Sakshi <sakshiyuvi92@gmail.com>
2026-09-18 19:33:24 +0530

    Add Is Subsequence Solution

---
+++ Easy/0392-Is-Subsequence.java
@@ -0,0 +1,17 @@
+// LeetCode 392: Is Subsequence
+// https://leetcode.com/problems/is-subsequence/
+
+class Solution {
+
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
