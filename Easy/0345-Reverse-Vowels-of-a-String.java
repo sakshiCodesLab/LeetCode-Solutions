8bdf28a170108c17f1df7b975414f8a7115d667e
Sakshi <sakshiyuvi92@gmail.com>
2026-09-20 22:37:30 +0530

    Add Reverse Vowels of a String Solution

---
+++ Easy/0345-Reverse-Vowels-of-a-String.java
@@ -0,0 +1,31 @@
+// LeetCode 345: Reverse Vowels of a String
+// https://leetcode.com/problems/reverse-vowels-of-a-string/
+
+class Solution {
+
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            if (!isVowel(arr[left])) {
+                left++;
+            } else if (!isVowel(arr[right])) {
+                right--;
+            } else {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+
+    public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+
+    }
+}
