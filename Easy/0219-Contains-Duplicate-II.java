cfb5c8b5f59c4d2114883e50a3b8780fba0ed7aa
Sakshi <sakshiyuvi92@gmail.com>
2026-09-25 19:35:26 +0530

    Add Contains Duplicate II Solution

---
+++ Easy/0219-Contains-Duplicate-II.java
@@ -0,0 +1,21 @@
+// LeetCode 219: Contains Duplicate II
+// https://leetcode.com/problems/contains-duplicate-II/
+
+import java.util.*;
+
+class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
